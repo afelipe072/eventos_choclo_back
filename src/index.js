1595fdf74004213fb83d951918fce6a696a15575
afelipe072 <andres.rojas.perez@correounivalle.edu.co>
2021-09-12 00:13:28 -0500

    nodemon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from './app'
 
 async function main (){
-    const port =process.env.PORT;
+    const port =process.env.PORT || 5000;
     await app.listen(port,()=>{
         console.log('Server on port ' + port);
     });
