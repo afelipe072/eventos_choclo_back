67354ff7d637697d84eed0e659116ece4430bf5e
afelipe07 <afelipe072@gmail.com>
2021-09-11 23:08:28 -0500

    primer comentario

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express, {json} from 'express';
+import morgan from 'morgan';
+import cors from 'cors'
+import pkg from '../package.json'
+
+//import routes
+import usuariosRoutes from './routes/usuarios';
+
+const app = express();
+
+app.set('pkg',pkg);
+
+//middleware
+app.use(morgan('dev'));
+app.use(json());
+app.use(cors());
+
+
+//routes
+app.get('/',(req,res)=>{
+    res.json(
+        {
+            author: app.get('pkg').author,
+            name: app.get('pkg').name,
+            version: app.get('pkg').version  
+        }
+    );
+});
+
+app.use(usuariosRoutes);
+
+
+export default app;
